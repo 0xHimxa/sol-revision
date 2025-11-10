696ac12125fe208dc24b93d946dad03d76468bb4
0xHimxa <himxa0x@gmail.com>
2025-11-10 02:32:42 +0100

    lernerd about deploying our contract and using it value from another conteact

---
+++ storeF/storagefactory.sol
@@ -0,0 +1,52 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.30;
+
+
+//over here we deploy simplestorgr in another contract
+//firt import it over her
+
+import {SimpleStroge} from '../simpleSttorage.sol';
+
+
+
+
+
+
+contract StorageFactory{
+
+// now let first create a value that store it
+
+ SimpleStroge[] public listofsimpleStorage;
+ 
+
+
+ // now we create a fn that deploy it
+
+ function deploySimpleStorage() public{
+
+//  the new keyword tell sol we want to deploy then we call the contract like fn
+   SimpleStroge newSimoleStoreage = new SimpleStroge();
+
+   // now this line add the deploed contract info and addres to the list above
+   listofsimpleStorage.push(newSimoleStoreage);
+
+
+
+
+ }
+
+
+ function sfStore(uint256 _index, uint256 _favnum)public {
+
+    // to interact with a contract we need it ABI and addrees whihc both are pushed to the listofsimplestorage
+   SimpleStroge getSimplestorage = listofsimpleStorage[_index];
+   getSimplestorage.store(_favnum);
+
+ }
+
+function sfRetrive(uint256 _index)public view  returns(uint256) {
+ SimpleStroge getSimplestorage = listofsimpleStorage[_index];
+  return getSimplestorage.retrived();
+}
+}
+
