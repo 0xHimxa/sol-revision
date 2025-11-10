5a4110ec587f5e4d3e5cfc515c13b1e53d9403e3
0xHimxa <himxa0x@gmail.com>
2025-11-10 01:20:11 +0100

    created contact save contract

---
+++ contanct.sol
@@ -0,0 +1,32 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.30;
+
+contract Savecontact {
+    struct UserInfo {
+        string name;
+        string number;
+        string email;
+      
+    }
+
+    UserInfo[] public userInfo;
+
+mapping(string => UserInfo) public userInfoByName;
+
+    function createUser(
+        string memory _name,
+        string memory _number,
+        string memory _email
+      
+    ) public {
+        UserInfo memory user = UserInfo({
+            name: _name,
+            number: _number,
+            email: _email
+        });
+
+        userInfo.push(user);
+        userInfoByName[_name] = user;
+
+    }
+}
