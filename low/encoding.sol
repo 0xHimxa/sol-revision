36fd8329da25b98185a1568065dffabfedddf61f
0xHimxa <himxa0x@gmail.com>
2025-12-10 10:44:51 +0100

    cover more about decoding and started to learn more about call

--- low/encoding.sol
+++ low/encoding.sol
@@ -22,8 +22,15 @@ contract Encoding{
 // byte type casting do similar with encodePacked check the diiff ask Ai
 
  function combineStrigBytes() external pure returns(bytes memory){
+    //bytes can not encode multiple string
         return bytes('hello world');
     }
+
+
+//note for encodePacked and bytes we can just typecast to string and it will decode it
+//this encode below only for for abi.encode
+
+
 
 //we can use it for what is in bytes form
 
@@ -36,6 +43,40 @@ string memory world = abi.decode(combineString(),(string));
 return world;
 }
 
+
+function multiEncode() public pure returns(bytes memory){
+bytes memory name = abi.encode('hello world','i am himxa');
+return name;
+
+}
+
+
+function multiDecode() external pure returns(string memory,string memory){
+//since we encode to string  to acces the two value we do it like below
+
+(string memory world,string memory intro) = abi.decode(multiEncode(),(string,string));
+
+return(world,intro);
+
+
+}
+
+
+// some more low level stuff
+ 
+ // call: how we can chaing the state of a blocchian;
+
+ // delegatecall: how we can change the state of a blockchain with another contract;
+ 
+ // staticcall: how we can read the state of a blockchain;
+
+
+ function withdraw( address winner) external returns(bool){
+    // in the place we use call to populate the value field of the transaction the amount to be sent
+    // while if we pass in daata in () to call a specific function
+    (bool success,) = winner.call{value: address(this).balance}("");
+    return success;
+ }
 
 
 }
